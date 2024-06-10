34004059d33b4ba776d34b8976278af45a599993
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-06-10 16:20:44 +0300

    add Login Page

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login Form</title>
+    <link rel="stylesheet" href="src/style/style.css">
+</head>
+<body>
+    <div class="form-container">
+        <form action="" method="post">
+            <h3>login now</h3>
+            <input type="email" name="email" required placeholder="enter your email">
+            <input type="password" name="password" required placeholder="enter your password">
+            <input type="submit" name="submit" value="login now" class="form-btn">
+            <p>Don't have an account? <a href="register.php">register now</a></p>
+        </form>
+    </div>
+</body>
+</html>
