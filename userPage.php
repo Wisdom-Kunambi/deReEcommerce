dcf1b7382e71b6d7c89c62e39702dc227afff927
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-06-10 15:09:04 +0300

    add User Page

--- userPage.php
+++ userPage.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Page</title>
+    <title> User Page</title>
     <link rel="stylesheet" href="src/style/style.css">
 </head>
 <body>
 
 <div class="container">
     <div class="content">
-        <h3>hi, <span>Admin</span></h3>
+        <h3>hi, <span>User</span></h3>
         <h1>welcome <span></span></h1>
-        <p>this is an admin page</p>
+        <p>this is an user page</p>
         <a href="login.php" class="btn">login</a>
         <a href="register.php" class="btn">register</a>
         <a href="logout.php" class="btn">logout</a>
