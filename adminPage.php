13db5a2f498ec3d87659413d21477fabcbf5d8d8
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-06-10 15:02:13 +0300

    add Admin Page

--- adminPage.php
+++ adminPage.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
+    <link rel="stylesheet" href="src/style/style.css">
 </head>
 <body>
-    
+
+<div class="container">
+    <div class="content">
+        <h3>hi, <span>admin</span></h3>
+        <h1>welcome <span></span></h1>
+        <p>this is an admin page</p>
+        <a href="login.php" class="btn">login</a>
+        <a href="register.php" class="btn">register</a>
+        <a href="logout.php" class="btn">logout</a>
+    </div>
+</div>
+
 </body>
 </html>
